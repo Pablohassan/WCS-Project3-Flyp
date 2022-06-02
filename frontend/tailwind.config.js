f53451f950fe6f4901b9292d30da74b2f7ccc3d3
PabloHassan <95355922+Pablohassan@users.noreply.github.com>
2022-06-02 15:29:40 +0200

    tailwin (#2)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
