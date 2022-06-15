af834b18905d31263f8680f244d5a6fa78813438
Vincent Vaur <vaur.vincent@gmail.com>
2022-06-15 10:52:16 +0200

    Fix API in production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:5000";
 
 export const getContenu = async () => {
   return (await axios.get(`${API_URL}/cardlist`)).data;
